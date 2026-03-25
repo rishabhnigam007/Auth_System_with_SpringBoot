464ea34229324e0fa3d8367b4859b3cec3de41d3
Rishabh Nigam <rishabhakans@gmail.com>
2026-03-25 21:33:17 +0530

    JWT dependency added in pom.xml and start creating JwtService class

--- src/main/java/com/substring/auth/security/JwtService.java
+++ src/main/java/com/substring/auth/security/JwtService.java
@@ -1,9 +1,24 @@
 package com.substring.auth.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
+    //    private final SecretKey key;
+    private final long accessTtlSeconds;
+    private final long refreshTtlSeconds;
+    private final String issuer;
 
+
+    public JwtService(
+            @Value("${security.jwt.access-ttl-seconds}") long accessTtlSeconds,
+            @Value("${security.jwt.refresh-ttl-seconds}") long refreshTtlSeconds,
+            @Value("${security.jwt.issuer}") String issuer) {
+
+        this.accessTtlSeconds = accessTtlSeconds;
+        this.refreshTtlSeconds = refreshTtlSeconds;
+        this.issuer = issuer;
+    }
 }
